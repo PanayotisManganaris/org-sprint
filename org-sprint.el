00b2029d6b3859d75e66adc7fe3ab5541b444b32
Panayotis Manganaris <panos.manganaris@gmail.com>
2024-03-17 14:35:16 -0400

    GPL boilerplate

--- org-sprint.el
+++ org-sprint.el
@@ -1,4 +1,29 @@
 ;;; org-sprint.el --- an org-agenda overhaul -*- coding: utf-8; lexical-binding: t; -*-
+
+;; Copyright TODO
+
+;; Author: Panayotis Manganaris
+;; URL: https://github.com/PanayotisManganaris/org-spint
+;; Keywords: org, mode, sprint, agenda
+;; Version: 0.0.0
+;; Package-Requires: ((emacs "26.1") (org "9.4") (emacs-odbc "0.0.0"))
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
 ;;
 ;;; Code:
 

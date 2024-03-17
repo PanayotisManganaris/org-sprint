7b30f3d9f96501fa3a662574c789654aa2c53835
Panayotis Manganaris <panos.manganaris@gmail.com>
2024-03-17 14:29:00 -0400

    package boilerplate

---
+++ org-sprint.el
@@ -0,0 +1,10 @@
+;;; org-sprint.el --- an org-agenda overhaul -*- coding: utf-8; lexical-binding: t; -*-
+;;
+;;; Code:
+
+(require 'org-roam)
+(require 'emacs-odbc)
+
+(provide 'org-sprint)
+
+;;; org-sprint.el ends here
